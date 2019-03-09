c7f9b54f8dc358a794e8eb47a45441bf6e6cbfdc
saltwater404 <45968146+saltwater404@users.noreply.github.com>
2019-03-09 17:20:19 +0600

    AI Project Files

---
+++ Project/AI Project Code and required files/face_detect.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('1.jpg')
+face_csc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+faces = face_csc.detectMultiScale(gray,1.1,4)
+
+for(x,y,w,h) in faces:
+    cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),3)
+
+cv2.imshow('img',img)
+cv2.waitKey(0)
